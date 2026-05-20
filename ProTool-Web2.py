25c2b96dfa33dfbb0b54e3115e6df0b373f64eff
danielfgpontes <danielfgpontes@gmail.com>
2026-05-20 12:19:59 -0400

    Update ProTool-Web2.py

--- ProTool-Web2.py
+++ ProTool-Web2.py
@@ -12,7 +12,7 @@ import requests
 from pathlib import Path
 
 # --- CONFIGURAÇÃO INICIAL E MODELO ---
-MODEL = "gpt-4o-mini"
+MODEL = "gpt-5.4-mini"
 MODELO_ARQUIVO = "MODELO_Memorial_Planilha_Declaração_R00.xlsx"
 
 st.set_page_config(page_title="Análise Documental - Prefeituras", layout="wide")
